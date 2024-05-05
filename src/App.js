e3c41ad804d5d6b54ee9536821a3b1f1e3a117e2
yuuuuuuuuuda <iyedbehij815@gmail.com>
2024-05-05 12:34:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
+import products from './data/product.json';
+import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
-   <React.Fragment> <div>hello
-
-
+   <div > hello iyeed
+      <Navbar /> 
+      <div className="flex flex-wrap">
+      {products.products.map((product) => ( 
+        <div className="border border-red-500 my-5 w-90"> 
+          <h3 className="text-red-500">{product.title}</h3>
+          <img width="200" src={product.thumbnail} alt=" /" />
+            <p className="text-pink-500">{product.description}</p>
+            <small className="bg-gray-300">{product.price}$</small>
+          
+          </div>
+      ))}
+      </div>
     </div>
-    </React.Fragment>
+
+  
   );
 }
 
 export default App;
+
+
+
+
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+function Navbar() {
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <a href="">home</a>
+                </li>
+                <li>
+                    <a herf="">about</a>
+                </li>
+                <li>
+                <a herf="">contact</a>
+                </li>
+            </ul>
+        </nav>
+    )
+} 
+export default Navbar;
